cb7e76684f06f44c3b92d27fb201846e1cdc5d89
musekorea <musekorea@hanmail.net>
2021-09-03 15:26:58 +0800

    add title

--- kospi100_toExcel.py
+++ kospi100_toExcel.py
@@ -9,6 +9,13 @@ for page in range(1,2):
   print(res.status_code)
   soup = BeautifulSoup(res.text, "lxml")
   data_rows = soup.find("tbody").find_all("tr")
+  
+  f = open("kospi100.csv", mode="w", encoding="utf-8-sig", newline="")
+  fw = csv.writer(f)
+  title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE".split("\t")
+  print(title)
+  fw.writerow(title)
+  f.close()
     
   for row in data_rows:
     columns = row.find_all("td")
@@ -20,7 +27,7 @@ for page in range(1,2):
       if len(data)==0:
         continue
       results.append(data)
-        
+    
     f = open("kospi100.csv", mode="a", encoding="utf-8-sig", newline="")
     fw = csv.writer(f)
     fw.writerow(results)
